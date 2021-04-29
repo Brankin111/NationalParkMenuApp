7450dd9f323551507cb63928a69dd5c4efec7cd4
Jason Graham <jasongraham@cloudfitsoftware.com>
2021-04-29 12:00:01 -0400

    Example of how to use Newtonsoft for working with JSON.

---
+++ Location.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NationalParksMenuApp
+{
+    public class Location
+    {
+        [JsonProperty("coordinates")]
+        public List<double> Coordinates { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+}
---
+++ NationalPark.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NationalParksMenuApp
+{
+
+    public class NationalPark
+    {
+        [JsonProperty("LocationNumber")]
+        public string LocationNumber { get; set; }
+
+        [JsonProperty("LocationName")]
+        public string LocationName { get; set; }
+
+        [JsonProperty("Address")]
+        public string Address { get; set; }
+
+        [JsonProperty("City")]
+        public string City { get; set; }
+
+        [JsonProperty("State")]
+        public string State { get; set; }
+
+        [JsonProperty("ZipCode")]
+        public string ZipCode { get; set; }
+
+        [JsonProperty("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("FaxNumber")]
+        public string FaxNumber { get; set; }
+
+        [JsonProperty("Latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("Longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("Location")]
+        public Location Location { get; set; }
+    }
+}
